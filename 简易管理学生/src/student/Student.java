7cdc72ab94ae187714da0230826b27fe2b3786c4
chenzj-tanpp <54852189+chenzj-tanpp@users.noreply.github.com>
2019-11-11 21:41:19 +0800

    Add files via upload

---
+++ 简易管理学生/src/student/Student.java
@@ -0,0 +1,34 @@
+package student;
+
+public class Student {
+	private String id;
+	private String name;
+	private String sex;
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getsex() {
+		return sex;
+	}
+
+	public void setsex(String sex) {
+		this.sex = sex;
+	}
+	public Student() {
+		super();
+	}
+
+}
