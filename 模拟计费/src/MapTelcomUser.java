fff2d1645241b64f337e5c6aa145ec03e60e9723
chenzj-tanpp <54852189+chenzj-tanpp@users.noreply.github.com>
2019-12-07 16:48:22 +0800

    Add files via upload

--- 模拟计费/src/MapTelcomUser.java
+++ 模拟计费/src/MapTelcomUser.java
@@ -5,7 +5,8 @@ public class MapTelcomUser {
 
 	private String phoneNumber;
 	private String callTo;
-	private HashMap singleRecord;
+	private LinkedHashMap singleRecord;
+	private TreeMap TreeSingleRecord;
 	private ArrayList communicationRecords;
 
 	public MapTelcomUser(String phoneNumber) {
@@ -18,19 +19,19 @@ public class MapTelcomUser {
 	void generateCommunicateRecord() {
 		int recordNum = new Random().nextInt(10);
 		for (int i = 0; i <= recordNum; i++) {
-			this.singleRecord = new HashMap();
+			this.TreeSingleRecord = new TreeMap();
 			// 开始时间，当前时间之前的某个随机时间
 			long timeStart = System.currentTimeMillis() - new Random().nextInt(36000000);
 			// 结束时间开始后的十分钟内随机的一-个时间，至少一分钟
 			long timeEnd = timeStart + 60000 + new Random().nextInt(600000);
 			this.callTo = getCallToPhoneNumber();// 被叫号码
 			// 插入通话记录
-			this.singleRecord.put("主叫", this.phoneNumber);
-			this.singleRecord.put("开始时间", new Date(timeStart));
-			this.singleRecord.put("结束时间", new Date(timeEnd));
-			this.singleRecord.put("被叫号码", this.callTo);
-			this.singleRecord.put("计费", this.accountFee(timeStart, timeEnd));
-			this.communicationRecords.add(singleRecord);
+			this.TreeSingleRecord.put("主叫", this.phoneNumber);
+			this.TreeSingleRecord.put("开始时间", new Date(timeStart));
+			this.TreeSingleRecord.put("结束时间", new Date(timeEnd));
+			this.TreeSingleRecord.put("被叫号码", this.callTo);
+			this.TreeSingleRecord.put("计费", this.accountFee(timeStart, timeEnd));
+			this.communicationRecords.add(TreeSingleRecord);
 
 		}
 
@@ -56,17 +57,15 @@ public class MapTelcomUser {
 		Iterator it = this.communicationRecords.iterator();
 		while (it.hasNext()) {
 			System.out.println("----------通话记录分割线----------");
-			this.singleRecord=((HashMap)it.next());
-			Set entrySet = this.singleRecord.entrySet();
+			this.TreeSingleRecord = ((TreeMap) it.next());
+			Set entrySet = this.TreeSingleRecord.entrySet();
 			Iterator itEntry = entrySet.iterator();
 			while (itEntry.hasNext()) {
 				Map.Entry entry =(Map.Entry)itEntry.next();
 				System.out.println(entry.getKey() + ":" + entry.getValue());
 			}
 
-
-
-	}
+		}
 
 	}
 }
